ad8a4aebb48ae2595a80a0752617def03ca2d5b6
kevin.thomas <kevin.thomas@equinox.co.nz>
2019-01-24 09:40:22 +1300

    Added validation attributes to DTO classes.

--- RefactorThis/ApiModels/ProductDto.cs
+++ RefactorThis/ApiModels/ProductDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RefactorThis.ApiModels
 {
@@ -6,8 +7,11 @@ namespace RefactorThis.ApiModels
     {
         public Guid Id { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
 
+        [StringLength(500)]
         public string Description { get; set; }
 
         public decimal Price { get; set; }
--- RefactorThis/ApiModels/ProductOptionDto.cs
+++ RefactorThis/ApiModels/ProductOptionDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RefactorThis.ApiModels
 {
@@ -8,8 +9,11 @@ namespace RefactorThis.ApiModels
 
         public Guid ProductId { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
 
+        [StringLength(500)]
         public string Description { get; set; }
     }
 }
