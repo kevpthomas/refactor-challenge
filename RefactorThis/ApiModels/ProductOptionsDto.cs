616433815fe4660f7023465c8c317d6b743af5f3
kevin.thomas <kevin.thomas@equinox.co.nz>
2019-01-24 09:30:50 +1300

    Add API DTO classes for ProductOption.

---
+++ RefactorThis/ApiModels/ProductOptionsDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RefactorThis.ApiModels
+{
+    public class ProductOptionsDto
+    {
+        public IEnumerable<ProductOptionDto> Items { get; set; }
+    }
+}
