fc16158facdd0a7f8b0e9d837352518a644cdda2
kevin.thomas <kevin.thomas@equinox.co.nz>
2019-01-24 13:14:16 +1300

    Refactor to ensure API exceptions are captured without leaking framework details.

--- RefactorThis/Controllers/ProductsController.cs
+++ RefactorThis/Controllers/ProductsController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net;
 using System.Web.Http;
 using AutoMapper;
 using RefactorThis.ApiModels;
@@ -12,7 +11,8 @@ namespace RefactorThis.Controllers
     {
         /*
          * DONE Refactor to return my DTO objects wrapped in IHttpActionResult
-         * Then refactor to return generic error if anything goes wrong
+         * DONE Then refactor to return generic error if anything goes wrong
+         * Then refactor to use a stub logger
          * Then refactor to repository
          * Then refactor to correct HTTP codes, time permitting
          *     e.g., code 422 for bad ID submission (benandel.com)
@@ -22,36 +22,48 @@ namespace RefactorThis.Controllers
         [HttpGet]
         public IHttpActionResult GetAll()
         {
-            var products = new Products();
-            return Ok(Mapper.Map<ProductsDto>(products));
+            return ProcessRequestAndHandleException(() =>
+            {
+                var products = new Products();
+                return Ok(Mapper.Map<ProductsDto>(products));
+            });
         }
 
         [Route("search")]
         [HttpGet]
         public IHttpActionResult SearchByName(string name)
         {
-            var products = new Products(name);
-            return Ok(Mapper.Map<ProductsDto>(products));
+            return ProcessRequestAndHandleException(() =>
+            {
+                var products = new Products(name);
+                return Ok(Mapper.Map<ProductsDto>(products));
+            });
         }
 
         [Route("{id}")]
         [HttpGet]
         public IHttpActionResult GetProduct(Guid id)
         {
-            var product = new Product(id);
-            if (product.IsNew)
-                return NotFound();
+            return ProcessRequestAndHandleException(() =>
+            {
+                var product = new Product(id);
+                if (product.IsNew)
+                    return NotFound();
 
-            return Ok(Mapper.Map<ProductDto>(product));
+                return Ok(Mapper.Map<ProductDto>(product));
+            });
         }
 
         [Route]
         [HttpPost]
         public IHttpActionResult Create(Product product)
         {
-            product.Save();
+            return ProcessRequestAndHandleException(() =>
+            {
+                product.Save();
             
-            return Ok(Mapper.Map<ProductDto>(product));
+                return Ok(Mapper.Map<ProductDto>(product));
+            });
         }
 
         // TODO: remove Id from Product body
@@ -59,46 +71,58 @@ namespace RefactorThis.Controllers
         [HttpPut]
         public IHttpActionResult Update(Guid id, Product product)
         {
-            var orig = new Product(id)
+            return ProcessRequestAndHandleException(() =>
             {
-                Name = product.Name,
-                Description = product.Description,
-                Price = product.Price,
-                DeliveryPrice = product.DeliveryPrice
-            };
+                var orig = new Product(id)
+                {
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price,
+                    DeliveryPrice = product.DeliveryPrice
+                };
 
-            if (!orig.IsNew)
-                orig.Save();
+                if (!orig.IsNew)
+                    orig.Save();
             
-            return Ok(Mapper.Map<ProductDto>(orig));
+                return Ok(Mapper.Map<ProductDto>(orig));
+            });
         }
 
         [Route("{id}")]
         [HttpDelete]
         public IHttpActionResult Delete(Guid id)
         {
-            var product = new Product(id);
-            product.Delete();
-            return Ok();
+            return ProcessRequestAndHandleException(() =>
+            {
+                var product = new Product(id);
+                product.Delete();
+                return Ok();
+            });
         }
 
         [Route("{productId}/options")]
         [HttpGet]
         public IHttpActionResult GetOptions(Guid productId)
         {
-            var productOptions = new ProductOptions(productId);
-            return Ok(Mapper.Map<ProductOptionsDto>(productOptions));
+            return ProcessRequestAndHandleException(() =>
+            {
+                var productOptions = new ProductOptions(productId);
+                return Ok(Mapper.Map<ProductOptionsDto>(productOptions));
+            });
         }
 
         [Route("{productId}/options/{id}")]
         [HttpGet]
         public IHttpActionResult GetOption(Guid productId, Guid id)
         {
-            var option = new ProductOption(id);
-            if (option.IsNew)
-                return NotFound();
+            return ProcessRequestAndHandleException(() =>
+            {
+                var option = new ProductOption(id);
+                if (option.IsNew)
+                    return NotFound();
 
-            return Ok(Mapper.Map<ProductOptionDto>(option));
+                return Ok(Mapper.Map<ProductOptionDto>(option));
+            });
         }
 
         // TODO: remove ProductId from ProductOption body
@@ -106,10 +130,13 @@ namespace RefactorThis.Controllers
         [HttpPost]
         public IHttpActionResult CreateOption(Guid productId, ProductOption option)
         {
-            option.ProductId = productId;
-            option.Save();
+            return ProcessRequestAndHandleException(() =>
+            {
+                option.ProductId = productId;
+                option.Save();
 
-            return Ok(Mapper.Map<ProductOptionDto>(option));
+                return Ok(Mapper.Map<ProductOptionDto>(option));
+            });
         }
 
         // TODO: remove Id and ProductId from ProductOption body
@@ -117,25 +144,57 @@ namespace RefactorThis.Controllers
         [HttpPut]
         public IHttpActionResult UpdateOption(Guid id, ProductOption option)
         {
-            var orig = new ProductOption(id)
+            return ProcessRequestAndHandleException(() =>
             {
-                Name = option.Name,
-                Description = option.Description
-            };
+                var orig = new ProductOption(id)
+                {
+                    Name = option.Name,
+                    Description = option.Description
+                };
 
-            if (!orig.IsNew)
-                orig.Save();
+                if (!orig.IsNew)
+                    orig.Save();
 
-            return Ok(Mapper.Map<ProductOptionDto>(orig));
+                return Ok(Mapper.Map<ProductOptionDto>(orig));
+            });
         }
 
         [Route("{productId}/options/{id}")]
         [HttpDelete]
         public IHttpActionResult DeleteOption(Guid id)
         {
-            var opt = new ProductOption(id);
-            opt.Delete();
-            return Ok();
+            return ProcessRequestAndHandleException(() =>
+            {
+                var opt = new ProductOption(id);
+                opt.Delete();
+                return Ok();
+            });
+        }
+
+        /// <summary>
+        /// Simple method to provide standardised exception handling for all requests.
+        /// </summary>
+        /// <param name="processRequest">
+        /// Function to provide happy path request processing.
+        /// </param>
+        /// <returns>
+        /// Output from happy path request processing, or an internal server error in the event
+        /// of an unhandled exception.
+        /// </returns>
+        /// <remarks>
+        /// This method ensures no leakage of sensitive framework details.
+        /// </remarks>
+        private IHttpActionResult ProcessRequestAndHandleException(Func<IHttpActionResult> processRequest)
+        {
+            try
+            {
+                return processRequest();
+            }
+            catch
+            {
+                //TODO: catch and log the exception
+                return InternalServerError();
+            }
         }
     }
 }
