7cdfbf1dcfb48d45a00c4e49a855a59524a3c0d4
kevin.thomas <kevin.thomas@nvinteractive.com>
2019-01-24 23:28:23 +1300

    Add unit tests for ProductRepository.

---
+++ RefactorThis.Tests/Infrastructure/Data/ProductRepositoryTests/ListTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using NPoco;
+using NUnit.Framework;
+using RefactorThis.Core.Entities;
+using RefactorThis.Core.Exceptions;
+using RefactorThis.Infrastructure.Data;
+using RefactorThis.Infrastructure.Interfaces;
+using Shouldly;
+
+namespace RefactorThis.Tests.Infrastructure.Data.ProductRepositoryTests
+{
+    /// <summary>
+    /// Unit tests for the <see cref="ProductRepository"/> List method.
+    /// </summary>
+    /// <remarks>
+    /// Out of scope to test all repository methods. This demonstrates unit testing is possible
+    /// given the current architecture.
+    /// </remarks>
+    public class ListTests : UnitTestBase<ProductRepository>
+    {
+        [Test]
+        public void GivenAtLeastOneProductInDatabase()
+        {
+            var product = new Product
+            {
+                Id = Faker.Random.Guid(),
+                Name = Faker.Random.String(),
+                Description = Faker.Random.String(),
+                DeliveryPrice = Faker.Random.Decimal()
+            };
+
+            var database = CreateMock<IDatabase>();
+            database
+                .Setup(x => x.Fetch<Product>(It.IsNotNull<Sql>()))
+                .Returns(new List<Product> { product });
+
+            GetDependency<INPocoDatabaseFactory>().Setup(x => x.CreateDatabase()).Returns(database.Object);
+
+            TestInstance.List().ShouldContain(product);
+        }
+
+        [Test]
+        public void GivenDatabaseError()
+        {
+            var ex = new Exception(Faker.Random.String());
+
+            var database = CreateMock<IDatabase>();
+            database
+                .Setup(x => x.Fetch<Product>(It.IsNotNull<Sql>()))
+                .Throws(ex);
+
+            GetDependency<INPocoDatabaseFactory>().Setup(x => x.CreateDatabase()).Returns(database.Object);
+
+            Should.Throw<DataException>(() => TestInstance.List())
+                .InnerException.ShouldBe(ex);
+        }
+    }
+}
---
+++ RefactorThis.Tests/UnitTestBase.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+
+namespace RefactorThis.Tests
+{
+    [TestFixture]
+    public abstract class UnitTestBase<TUnderTest>
+        where TUnderTest : class
+    {
+        protected TUnderTest TestInstance { get; private set; }
+
+        protected AutoMocker AutoMocker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            AutoMocker = new AutoMocker(MockBehavior.Loose);
+            TestInstance = AutoMocker.CreateInstance<TUnderTest>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AutoMocker = null;
+            TestInstance = null;
+        }
+
+        protected Faker Faker => new Faker();
+
+        protected Mock<T> CreateMock<T>(MockBehavior mockBehaviour = MockBehavior.Loose) where T : class
+        {
+            return new Mock<T>(mockBehaviour);
+        }
+
+        protected Mock<T> CreateMock<T>(MockBehavior mockBehaviour = MockBehavior.Loose, params object[] args) where T : class
+        {
+            return new Mock<T>(mockBehaviour, args);
+        }
+
+        protected Mock<T> GetDependency<T>() where T : class 
+        {
+            return AutoMocker.GetMock<T>();
+        }
+    }
+}
