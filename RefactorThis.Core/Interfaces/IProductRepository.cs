f910709ef5b0db9986bd3736e405a2d349187841
kevin.thomas <kevin.thomas@equinox.co.nz>
2019-01-24 14:51:28 +1300

    Add NPoco. Add IProductRepository.

---
+++ RefactorThis.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using RefactorThis.Core.Entities;
+
+namespace RefactorThis.Core.Interfaces
+{
+    /// <summary>
+    /// Describes CRUD functionality for Product entities.
+    /// </summary>
+    public interface IProductRepository
+    {
+        IEnumerable<ProductEntity> List();
+
+        ProductEntity GetById(Guid id);
+        IEnumerable<ProductEntity> GetByName(string name);
+
+        ProductEntity Add(ProductEntity entity);
+        ProductEntity Update(ProductEntity entity);
+        ProductEntity Delete(ProductEntity entity);
+    }
+}
