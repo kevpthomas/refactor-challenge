30b62e0f1a351d7e9cc4d267723e8eeff800c30b
kevin.thomas <kevin.thomas@nvinteractive.com>
2019-01-25 00:09:04 +1300

    Unit tests added for ProductsController

---
+++ RefactorThis.Tests/Web/Controllers/ControllersUnitTestBase.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace RefactorThis.Tests.Web.Controllers
+{
+    public abstract class ControllersUnitTestBase<TUnderTest> : UnitTestBase<TUnderTest>
+        where TUnderTest : class
+    {
+        [OneTimeSetUp]
+        public void SetUpFixture()
+        {
+            AutoMapperConfig.ConfigureMappings();
+        }
+    }
+}
