806605d18b34ebcfc88b7fe16f318d51c6523707
kevin.thomas <kevin.thomas@xero.com>
2019-03-08 17:03:13 +1300

    Enhance UnitTestBase: create separate definition without a TestInstance; add SetDependency.

--- RefactorThis.Tests/UnitTestBase.cs
+++ RefactorThis.Tests/UnitTestBase.cs
@@ -6,27 +6,8 @@ using NUnit.Framework;
 namespace RefactorThis.Tests
 {
     [TestFixture]
-    public abstract class UnitTestBase<TUnderTest>
-        where TUnderTest : class
+    public abstract class UnitTestBase
     {
-        protected TUnderTest TestInstance { get; private set; }
-
-        protected AutoMocker AutoMocker;
-
-        [SetUp]
-        public void SetUp()
-        {
-            AutoMocker = new AutoMocker(MockBehavior.Loose);
-            TestInstance = AutoMocker.CreateInstance<TUnderTest>();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            AutoMocker = null;
-            TestInstance = null;
-        }
-
         protected Faker Faker => new Faker();
 
         protected Mock<T> CreateMock<T>(MockBehavior mockBehaviour = MockBehavior.Loose) where T : class
@@ -38,10 +19,42 @@ namespace RefactorThis.Tests
         {
             return new Mock<T>(mockBehaviour, args);
         }
+    }
+
+    public abstract class UnitTestBase<TUnderTest> : UnitTestBase
+        where TUnderTest : class
+    {
+        // this convention allows SetDependency to take effect before instantiating TestInstance
+        private TUnderTest _testInstance;
+        protected TUnderTest TestInstance
+        {
+            get => _testInstance ?? (_testInstance = AutoMocker.CreateInstance<TUnderTest>());
+            private set => _testInstance = value;
+        }
+
+        protected AutoMocker AutoMocker { get; private set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            AutoMocker = new AutoMocker(MockBehavior.Loose);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AutoMocker = null;
+            TestInstance = null;
+        }
 
         protected Mock<T> GetDependency<T>() where T : class 
         {
             return AutoMocker.GetMock<T>();
+        }
+
+        protected void SetDependency<T>(Mock<T> dependency) where T : class
+        {
+            AutoMocker.Use(dependency);
         }
     }
 }
